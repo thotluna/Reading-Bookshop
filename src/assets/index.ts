359d3eb3b7b11b2c00bfdb4560af9d22f986465c
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-08-14 18:29:49 -0400

    feat: :sparkles: Add icons files

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+export * from './error.svg';
+export * from './filter.svg';
+export * from './heart.svg';
+export * from './hide.svg';
+export * from './react.svg';
+export * from './search.svg';
+export * from './show.svg';
