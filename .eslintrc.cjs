452d23f2aff6bd6aeb2930c3e3d78879819e5b24
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-08-14 17:50:52 -0400

    chore: :heavy_plus_sign: Add prettier dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,13 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'eslint-config-prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'no-console': 'error'
+  }
 }
