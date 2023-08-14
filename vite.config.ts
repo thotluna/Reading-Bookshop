d04ab0e7f9bed556a9d664cc4c0e923d043c6f30
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-08-14 19:22:57 -0400

    chore: :wrench: Add alias path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,14 @@ export default defineConfig({
     globals: true,
     environment: 'happy-dom',
     setupFiles: ['./setupTest.ts']
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@mod-catalogue': path.resolve(__dirname, './src/modules/catalogue'),
+      '@mod-reading': path.resolve(__dirname, './src/modules/reading'),
+      '@sections': path.resolve(__dirname, './src/sections')
+    }
   }
 })
