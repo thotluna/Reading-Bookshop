d75fac11b3e0cecc822bb5e6d466f5407e2b5b22
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-08-16 12:01:23 -0400

    feat: :sparkles: Create filters components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { InMemoryCatalogueRepository } from '@mod-catalogue/infraestructure'
 import { CatalogueComponent } from '@sec-catalogue/CatalogueComponent'
 import { Header } from '@sections/header/Header'
+import { ReadingComponent } from './sections/reading/ReadingComponent'
+import { Filters } from './sections/Filters'
 
 function App() {
   const repository = InMemoryCatalogueRepository()
+
   return (
     <div className="w-full h-full flex flex-col">
-      <Header />
-      <CatalogueComponent repository={repository} />
+      <Header>
+        <Filters />
+      </Header>
+      <main className="container mx-auto">
+        <div className="w-full flex justify-between">
+          <CatalogueComponent repository={repository} />
+          <ReadingComponent />
+        </div>
+      </main>
     </div>
   )
 }
--- src/sections/header/Header.tsx
+++ src/sections/header/Header.tsx
@@ -1,7 +1,12 @@
-export function Header() {
+interface Props {
+  children: JSX.Element | JSX.Element[]
+}
+
+export function Header({ children }: Props) {
   return (
-    <header className="w-full flex items-center justify-between px-2 md:px-8">
+    <header className="w-full flex items-end justify-between px-2 md:px-8">
       <h1 className="text-5xl md:text-8xl text-blue-400">Reading Bookshop</h1>
+      {children}
     </header>
   )
 }
