4c4a4dd3ba03c1a6c2c05b0532cb2fd244d24782
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-08-15 21:27:58 -0400

    feat: :construction_worker: Create Header component

---
+++ src/sections/header/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="w-full flex items-center justify-between px-2 md:px-8">
+      <h1 className="text-5xl md:text-8xl text-blue-400">Reading Bookshop</h1>
+    </header>
+  )
+}
